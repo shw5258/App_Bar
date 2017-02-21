e47df9f14e96d9066c98a65b6f524f7ed7dfae7d
Hyunwoo Song <shw5258@naver.com>
2017-02-21 16:27:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/light/appbar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.light.appbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+//https://developer.android.com/training/appbar/index.html
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+//        getSupportActionBar().hide();
+    }
+}
